3edc8b180dbfe6c679ac8c1a6f0129f7f82594c1
Tangocode <tangocode@MacBook-Air-de-Tangocode.local>
2017-02-22 20:32:21 -0300

    Add test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -245,7 +245,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                })
+                }).catch((e) => done(e));
             });
     });
 
@@ -277,3 +277,52 @@ describe('POST /users', () => {
             .end(done);
     });
 })
+
+describe ('POST /users/login', () => {
+    it ('should login user and return auth token', (done) => {
+        var email = users[1].email;
+        var password = users[1].password;
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(200)
+            .expect((response) => {
+                expect(response.headers['x-auth']).toExist();
+            })
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: response.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it ('should reject invalid login', (done) => {
+        var email = users[1].email;
+        var password = "WrongPassword"
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(400)
+            .expect((response) => {
+                expect(response.headers['x-auth']).toNotExist();
+            })
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
