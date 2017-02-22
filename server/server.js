e7740f4eb8d6af284fddc5632c705aba73a00b80
Tangocode <tangocode@MacBook-Air-de-Tangocode.local>
2017-02-22 01:15:30 -0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -66,6 +67,32 @@ app.delete('/todos/:id', (request, response) => {
     }).catch((error) => {
         response.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (request, response) => {
+    var id = request.params.id;
+    // Especifica qué propiedades puede actualizar el usuario
+    var body = _.pick(request.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id))
+    {
+        return response.status(404).send();
+    }
+    // Se setean los valores nuevos de las propiedades el objeto
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    // Se actualiza el objeto
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return response.status(404).send();
+        }
+        response.send({todo});
+    }).catch((error) => {
+        response.status(400).send();
+    })
 });
 
 app.listen(port, () => {
